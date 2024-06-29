8e7d94b8fa3a3d1627dbd20a1e1f5f5694583bde
Deepanraj K <deepanraj770848@gmail.com>
2024-06-29 12:55:10 +0530

    add

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,18 +1,57 @@
-import React, { createContext } from 'react'
-import all_product from '../Components/Assets/all_product'
-
+import React, { createContext, useState } from 'react';
+import all_products from '../Components/Assets/all_product';
 
 export const ShopContext = createContext(null);
 
-const ShopContextProvider = (props) => {
+const getDefaultCart = () => {
+  let cart = {};
+  for (let index = 0; index < all_products.length; index++) {
+    cart[all_products[index].id] = 0;
+  }
+  return cart;
+}
 
-  const contextValue = { all_product };
+const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+  const addToCart = (itemId) => {
+    console.log(`Adding item ${itemId} to cart`);
+    setCartItems((prev) => ({ ...prev, [itemId]: (prev[itemId] || 0) + 1 }));
+  }
+
+  const removeFromCart = (itemId) => {
+    console.log(`Removing item ${itemId} from cart`);
+    setCartItems((prev) => ({ ...prev, [itemId]: Math.max((prev[itemId] || 0) - 1, 0) }));
+  }
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_products.find((product) => product.id === Number(item));
+        totalAmount += itemInfo.new_price * cartItems[item];
+      }
+    }
+    return totalAmount;
+  }
+  
+  const getTotalCartItems = () => {
+    let totalItems = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItems += cartItems[item];
+      }
+    }
+    return totalItems;
+  }
+  
+
+  const contextValue = { all_products, cartItems, addToCart, removeFromCart, getTotalCartAmount, getTotalCartItems };
 
   return (
     <ShopContext.Provider value={contextValue}>
       {props.children}
     </ShopContext.Provider>
-  )
+  );
 }
 
 export default ShopContextProvider;
