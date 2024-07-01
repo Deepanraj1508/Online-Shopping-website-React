99d6842c401009b63be1e64d2093366582e2a283
Deepanraj K <deepanraj770848@gmail.com>
2024-07-01 11:58:21 +0530

    .

--- frontend/src/Components/Assets/new_collections.js
+++ frontend/src/Components/Assets/new_collections.js
@@ -1,11 +1,11 @@
-import p1_img from "./product_12.png";
-import p2_img from "./product_35.png";
-import p3_img from "./product_14.png";
-import p4_img from "./product_8.png";
-import p5_img from "./product_15.png";
-import p6_img from "./product_2.png";
-import p7_img from "./product_17.png";
-import p8_img from "./product_28.png";
+import p1_img from "./Products/product_12.png";
+import p2_img from "./Products/product_35.png";
+import p3_img from "./Products/product_14.png";
+import p4_img from "./Products/product_8.png";
+import p5_img from "./Products/product_15.png";
+import p6_img from "./Products/product_2.png";
+import p7_img from "./Products/product_17.png";
+import p8_img from "./Products/product_28.png";
 
 let new_collections = [
   {
